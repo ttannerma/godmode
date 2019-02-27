0b8e366467be682e4b3ba6afc17a461bd297f8a4
Teemu <teemu.tannerma@gmail.com>
2019-02-27 17:49:45 +0200

    Added !ping command, that bot replies.

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const { prefix, token } = require('./config.json')
 
 client.once('ready', () => {
-
+  console.log('ready!')
 })
+
+client.on('message', message => {
+  if (message.content === prefix + 'ping') {
+    console.log()
+    message.channel.send('Pong.')
+  }
+})
+
+client.login(token)
